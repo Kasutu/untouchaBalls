0d7b045dbf4c001f0d3d55d93fa135df46d05947
Diadem Grace Arroz <89431768+DiademGraceArroz@users.noreply.github.com>
2022-05-03 16:23:52 +0800

    added canvas class

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 // Diadem Grace Arroz, Jerome Cabugwason
 
 // loads resources and executes the main engine
-import GameLoop from './gameLoop';
+import GameLoop from "./gameLoop";
 
 const gl = new GameLoop();
 
@@ -14,3 +14,11 @@ function update(): void {}
 
 // draw when update is complete
 function render(): void {}
+
+let canvas = document.querySelector("canvas") as HTMLCanvasElement;
+let ctx = canvas.getContext("2d");
+
+export default class Canvas {
+  public canvas = document.querySelector("canvas") as HTMLCanvasElement;
+  public ctx = canvas.getContext("2d");
+}
---
+++ src/utils/canvas.ts
@@ -0,0 +1,7 @@
+let canvas = document.querySelector("canvas") as HTMLCanvasElement;
+let ctx = canvas.getContext("2d");
+
+export default class Canvas {
+  public canvas = document.querySelector("canvas") as HTMLCanvasElement;
+  public ctx = canvas.getContext("2d");
+}
